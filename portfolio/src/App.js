2c2b27e2e6ee0f52db8ef9e9651fdea2a5d0a4da
roselandroche <r.landroche@gmail.com>
2019-11-05 19:49:43 -0500

    Adding some components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      Hey
+      <Nav />
     </div>
   );
 }
